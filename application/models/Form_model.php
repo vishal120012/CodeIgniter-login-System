7bcc74ccbd74e4790721a997013ffef83244ed53
vishal120012 <44400127+vishal120012@users.noreply.github.com>
2020-01-11 15:50:31 +0530

    Update Form_model.php

--- application/models/Form_model.php
+++ application/models/Form_model.php
@@ -4,8 +4,7 @@ class Form_model extends CI_Model {
     public function __construct()
     {
         $this->load->database();
-    }
-     
+    }  
     public function auth_check($data)
     {
         $query = $this->db->get_where('users', $data);
